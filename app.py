06f7f39d8cdd1c4aaa0091896c5a0bc4f5ec0ac1
ab-mtz <bmtzs@me.com>
2023-10-29 19:08:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-import PIL
-import pytesseract # Not installed yet
+# import PIL
+# import 
 
+
+import matplotlib.pyplot as plt
+
+import keras_ocr
+
+# keras-ocr will automatically download pretrained
+# weights for the detector and recognizer.
+pipeline = keras_ocr.pipeline.Pipeline()
+
+# Get a set of three example images
+images = ['img-2.jpg', 'img-3.jpg']
+# [
+#     keras_ocr.tools.read(url) for url in [
+#         'https://upload.wikimedia.org/wikipedia/commons/b/bd/Army_Reserves_Recruitment_Banner_MOD_45156284.jpg',
+#         'https://upload.wikimedia.org/wikipedia/commons/e/e8/FseeG2QeLXo.jpg',
+#         'https://upload.wikimedia.org/wikipedia/commons/b/b4/EUBanana-500x112.jpg'
+#     ]
+# ]
+
+# Each list of predictions in prediction_groups is a list of
+# (word, box) tuples.
+prediction_groups = pipeline.recognize(images)
+
+# Plot the predictions
+fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
+for ax, image, predictions in zip(axs, images, prediction_groups):
+    keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
 # Steps
     # read pdf
     # if pdf multiple pages: iterate over it
